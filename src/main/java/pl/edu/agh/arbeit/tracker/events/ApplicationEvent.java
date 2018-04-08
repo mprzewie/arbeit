5537a32b672615463dbbac252b7fef4efb413e50
Marcin Przewięźlikowski <m.przewie@gmail.com>
2018-04-08 18:24:58 +0200

    Create Tracker interface Move packages around a bit; Create basic ApplicationTracker; Rename TrackedApplication to Application;

---
+++ src/main/java/pl/edu/agh/arbeit/tracker/events/ApplicationEvent.java
@@ -0,0 +1,25 @@
+package pl.edu.agh.arbeit.tracker.events;
+
+
+import pl.edu.agh.arbeit.tracker.Application;
+
+public class ApplicationEvent extends Event {
+
+    private final Application application;
+    private final EventType type;
+
+    public ApplicationEvent(EventType type, Application application) {
+        this.application = application;
+        this.type = type;
+    }
+
+    @Override
+    public String getTopic() {
+        return application.getName();
+    }
+
+    @Override
+    public EventType getType() {
+        return type;
+    }
+}
