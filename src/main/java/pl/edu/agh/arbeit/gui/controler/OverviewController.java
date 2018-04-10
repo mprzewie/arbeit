66da53f64fb56af1cc7956b69a190ca502676938
marwin1991 <peter.zmilczak@gmail.com>
2018-04-10 23:14:45 +0200

    reapired pom and made a working simple and empty GUI

---
+++ src/main/java/pl/edu/agh/arbeit/gui/controler/OverviewController.java
@@ -0,0 +1,34 @@
+package pl.edu.agh.arbeit.gui.controler;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import pl.edu.agh.arbeit.gui.Main;
+
+import java.io.IOException;
+
+public class OverviewController {
+    private Stage primaryStage;
+    private final String APP_TITLE = "DiceMaster";
+    private final String MAIN_PAIN_RPATH = "view/MainWindowPane.fxml";
+
+    public OverviewController(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        try {
+            this.primaryStage.setTitle(APP_TITLE);
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource(MAIN_PAIN_RPATH));
+            System.out.print(Main.class.getResource(MAIN_PAIN_RPATH));
+            AnchorPane rootLayout = loader.load();
+            MainWindowController mainWindowController = loader.getController();
+            mainWindowController.init(this);
+            Scene scene = new Scene(rootLayout);
+            this.primaryStage.setScene(scene);
+            this.primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
