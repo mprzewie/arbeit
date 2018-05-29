1c07d3e7c989f7f693a6729e39c4bb0e3af5f0ea
Marcin Przewięźlikowski <m.przewie@gmail.com>
2018-05-29 23:34:11 +0200

    Some fixes to CsvReport. Add more readable forEach, move creating DurationCalculators out from the loop iterating over dates. The most important bug was that for each date, results from *all* dates was stored in the row - a legacy from when we were storing results from dates in rows, not columns.

--- src/main/java/pl/edu/agh/arbeit/data/report/CsvReport.java
+++ src/main/java/pl/edu/agh/arbeit/data/report/CsvReport.java
@@ -3,6 +3,7 @@ package pl.edu.agh.arbeit.data.report;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 import pl.edu.agh.arbeit.data.repository.DatabaseEventRepository;
+import pl.edu.agh.arbeit.tracker.Application;
 import pl.edu.agh.arbeit.tracker.events.Event;
 import pl.edu.agh.arbeit.tracker.events.EventType;
 
@@ -24,7 +25,6 @@ public class CsvReport implements Report {
 //    private final Collection<Event> predecessors;
     private final List<LocalDate> dates;
     private final List<String> appsToReport;
-    private final String[] firstColumnArray;
    // private final CSVFormat format;
 
     public CsvReport(List<String> appsToReport, List<Event> events) {
@@ -33,16 +33,10 @@ public class CsvReport implements Report {
         this.events = events;
         // list of distinct LocalDates when systemEvents happened
         this.dates = getSortedRelevantEvents("system")
+                .stream()
                 .map(Event::getLocalDate)
                 .distinct()
                 .collect(Collectors.toList());
-
-        ArrayList<String> firstColumnList = new ArrayList<>();
-        firstColumnList.add("topic");
-        firstColumnList.add("activity");
-        firstColumnList.addAll(dates.stream().map(LocalDate::toString).distinct().collect(Collectors.toList()));
-        //this.firstColumnList = firstColumnList;
-        this.firstColumnArray = firstColumnList.toArray(new String[0]);
         //this.format = CSVFormat.DEFAULT.withHeader(header);
     }
 
@@ -54,16 +48,18 @@ public class CsvReport implements Report {
         LinkedList<String> record = new LinkedList<>();
 
         /* first row */
-        record.addLast(firstColumnArray[0]);
+        record.addLast("topic");
         appsToReport.forEach(app -> {
-            int i = 0;
-            while(i++ != 2) record.addLast(app);
+            // for ACTIVE column
+            record.addLast(app);
+            // for PASSIVE column
+            record.addLast(app);
         });
         printer.printRecord(record);
         record.clear();
 
         /* second row */
-        record.addLast(firstColumnArray[1]);
+        record.addLast("activity");
         appsToReport.forEach(app -> {
             record.addLast("ACTIVE");
             record.addLast("PASSIVE");
@@ -71,77 +67,45 @@ public class CsvReport implements Report {
         printer.printRecord(record);
         record.clear();
 
+        // create a DurationCalculator for each app only once
+        // no need to calculate the same stuff for each date
+        DatabaseEventRepository repo = new DatabaseEventRepository();
+
+        List<DurationCalculator> appDurationCalculators = appsToReport.stream()
+                .map(app -> {
+                    Optional<Event> previousEvent =
+                            repo.getPreviousEventTypeForApp(getSortedRelevantEvents(app));
+                    return new DurationCalculator(
+                            app,
+                            getSortedRelevantEvents(app),
+                            previousEvent
+                    );
+        }).collect(Collectors.toList());
+
         /* data rows */
-        for(int i = 2; i < firstColumnArray.length; i++) {
-            record.addLast(firstColumnArray[i]);
-
-            appsToReport.forEach(app -> {
-                DatabaseEventRepository repo = new DatabaseEventRepository();
-                Optional<Event> opt = repo.getPreviousEventTypeForApp(getSortedRelevantEvents(app)
-                        .collect(Collectors.toList()));
-                DurationCalculator calculator = new DurationCalculator(
-                        app,
-                        getSortedRelevantEvents(app)
-                                .collect(Collectors.toList()),
-                        opt
-                );
+        dates.forEach(date -> {
+            record.addLast(date.toString());
+            appDurationCalculators.forEach(calculator -> {
                 Arrays.asList(EventType.ACTIVE, EventType.PASSIVE)
-                        .forEach(activityType -> {
-                            record.addAll(
-                                    dates.stream()
-                                            .map(date -> calculator.activityLength(date, activityType))
-                                            .map(Duration::toMinutes)
-                                            .map(String::valueOf)
-                                            .collect(Collectors.toList()));
-
-                        });
+                        .forEach(activityType ->
+                                record.add(String.valueOf(
+                                                calculator.activityLength(date, activityType).toMinutes()
+                                        ))
+                        );
             });
-            printer.printRecord(record);
+            try {
+                printer.printRecord(record);
+            } catch (IOException ignored) {}
             record.clear();
-        }
+        });
 
         printer.close();
-//
-//        CSVFormat format = CSVFormat.EXCEL;
-//        try(CSVPrinter printer = new CSVPrinter(writer, format )){
-//            appsToReport.stream()
-//                    .distinct()
-//                    .forEach(topic -> {
-//                        DatabaseEventRepository repo = new DatabaseEventRepository(false);
-//                        Optional<Event> opt = repo.getPreviousEventTypeForApp(getSortedRelevantEvents(topic)
-//                                .collect(Collectors.toList()));
-//                        DurationCalculator calculator = new DurationCalculator(
-//                                topic,
-//                                getSortedRelevantEvents(topic)
-//                                        .collect(Collectors.toList()),
-//                                opt
-//                        );
-//                        Arrays.asList(EventType.ACTIVE, EventType.PASSIVE)
-//                                .forEach(activityType -> {
-//                                    LinkedList<String> record = new LinkedList<>();
-//                                    record.addLast(topic);
-//                                    record.addLast(activityType.toString());
-//
-//                                    record.addAll(
-//                                            dates.stream()
-//                                            .map(date -> calculator.activityLength(date, activityType))
-//                                            .map(Duration::toMinutes)
-//                                            .map(String::valueOf)
-//                                            .collect(Collectors.toList()));
-//                                        try {
-//                                            printer.printRecord(record);
-//                                        } catch (IOException e) {
-//                                        e.printStackTrace();
-//                                        }
-//                                });
-//                        });
-//        }
 
     }
 
     // This method returns a stream of events relevant to given topic sorted by datetime.
     // As SystemEvents are relevant to every topic, they are included there too
-    private Stream<Event> getSortedRelevantEvents(String topic){
+    private List<Event> getSortedRelevantEvents(String topic){
        return events
                 .parallelStream()
                 .filter(event -> event.getTopic().equals(topic))
@@ -149,7 +113,8 @@ public class CsvReport implements Report {
                     if(o1.getDateTime().isBefore(o2.getDateTime())) return -1;
                     else if(o2.getDateTime().isBefore(o1.getDateTime())) return 1;
                     else return 0;
-                });
+                })
+               .collect(Collectors.toList());
     }
 
     @Override
